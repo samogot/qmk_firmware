b1b9bec39aeb31684de10a7f9d9009e470adeddc
samogot <samogot@gmail.com>
2022-11-18 11:45:52 +0100

    Add script to fetch oryx versions into separate branch

---
+++ fetch_oryx.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+URL=$1
+BASENAME=${${URL##*/}%%.zip}
+VER=${BASENAME##*_}
+KB=${BASENAME%%_*}
+KM=${${BASENAME#*_}%_*_$VER}
+
+git checkout oryx || exit 1
+
+if [[ $(git log -n 1 --pretty='format:%s') == $VER ]]; then
+  echo Up to date
+  git checkout master
+  exit 0
+fi
+
+wget -c $URL || exit 1
+unzip -o -j $BASENAME "${KB}_${KM}_source/*" -d keyboards/$KB/keymaps/$KM
+rm $BASENAME.zip
+
+git commit -m "$VER" keyboards/$KB/keymaps/$KM/* || exit 1
+git checkout master
